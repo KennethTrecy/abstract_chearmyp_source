3d29afaa8ff60b5c61a01c922203eb6854e10388
Kenneth Trecy Tobias <33564322+KennethTrecy@users.noreply.github.com>
2021-08-07 20:46:45 +0800

    Add crate-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
 #![cfg_attr(feature = "no_std", no_std)]
 
+//! # Chearmyp Abstract Source
+//! Please read the README.md for more information.
+//!
+//! ## Features available
+//! - `str_source`: Implements [AbstractSource] for [str].
+//! - `slice_u8_source`: Implements [AbstractSource] for `&[u8]` [slice].
+//! - `no_std`: Uses the `core` crate instead of `std` crate.
+
 mod abstract_source;
 
 pub use abstract_source::AbstractSource;
