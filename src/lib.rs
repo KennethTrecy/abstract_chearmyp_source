7bd255a1f779c1edde69e9b769f042dcce4dabe2
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-18 16:15:32 +0800

    Update the root of the library

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,22 @@
 	doc = "- `vec_source_collection`: Implements [AbstractSourceCollection] for [Vec].",
 )]
 //!- `no_std`: Uses the `core` crate instead of `std` crate.
+//! - `str_comparable_to_u8`. Implements [ComparableAbstractSource] for &[str] allowing
+//!   comparison(s) to [u8].
+//! - `str_comparable_to_str`. Implements [ComparableAbstractSource] for &[str] allowing
+//!   comparison(s) to &[str].
+//! - `slice_u8_comparable_to_u8`. Implements [ComparableAbstractSource] for `&[u8]` [slice]
+//!   allowing comparison(s) to [u8].
+//! - `slice_u8_comparable_to_str`. Implements [ComparableAbstractSource] for `&[u8]` [slice]
+//!   allowing comparison(s) to &[str].
 
 #[cfg(feature = "no_std")]
 extern crate alloc;
 
 mod abstract_source;
 mod abstract_source_collection;
+mod comparable_abstract_source;
 
 pub use abstract_source::AbstractSource;
 pub use abstract_source_collection::AbstractSourceCollection;
+pub use comparable_abstract_source::ComparableAbstractSource;
