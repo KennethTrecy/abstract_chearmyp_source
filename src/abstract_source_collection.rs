ffe1a3c93d20b6083e51c8fedf502b44df87183f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-18 16:13:33 +0800

    Specify the types by using generic syntax
    Library user is now responsible for the creation of source collection.

--- src/abstract_source_collection.rs
+++ src/abstract_source_collection.rs
@@ -1,18 +1,12 @@
 use crate::AbstractSource;
 
 /// An abstraction of source collection.
-pub trait AbstractSourceCollection {
-	/// The type of source the collection contains.
-	type Source: AbstractSource;
-
-	/// Creates an empty source collection.
-	fn new() -> Self;
-
+pub trait AbstractSourceCollection<T: AbstractSource> {
 	/// Gets the source at the specified index.
-	fn get_source(&self, _: usize) -> Option<&Self::Source>;
+	fn get_source(&self, _: usize) -> Option<&T>;
 
 	/// Add the source into the collection.
-	fn add_source(&mut self, _: Self::Source);
+	fn add_source(&mut self, _: T);
 }
 
 #[cfg(feature = "vec_source_collection")]
