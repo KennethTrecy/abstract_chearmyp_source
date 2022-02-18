4eec0d475ec238dfc12ecca151dcb014a793fed4
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-18 16:10:43 +0800

    Update current implementors of abstract source

--- src/abstract_source/slice_u8.rs
+++ src/abstract_source/slice_u8.rs
@@ -2,18 +2,16 @@ use crate::AbstractSource;
 
 /// This is only available if `slice_u8_source` feature has been activated.
 impl<'a> AbstractSource for &'a [u8] {
-	type Slice = &'a [u8];
-
-	fn forward_slice(&self, start_index: usize) -> Self::Slice {
+	fn forward_slice(self, start_index: usize) -> Self {
 		&self[start_index..]
 	}
 
-	fn is_empty(&self) -> bool {
-		self.len() == 0
+	fn slice(self, start_index: usize, stop_index: usize) -> Self {
+		&self[start_index..stop_index]
 	}
 
-	fn is_equal_at(&self, index: usize, byte: u8) -> bool {
-		self.get(index) == Some(&byte)
+	fn is_empty_at(&self, index: usize) -> bool {
+		index >= self.len()
 	}
 }
 
@@ -47,38 +45,5 @@ mod t {
 		let is_empty = source.is_empty();
 
 		assert_eq!(is_empty, false);
-	}
-
-	#[test]
-	fn should_be_equal() {
-		let source = &b"d"[..];
-		let index = 0;
-		let byte = 'd' as u8;
-
-		let is_equal = source.is_equal_at(index, byte);
-
-		assert_eq!(is_equal, true);
-	}
-
-	#[test]
-	fn should_not_be_equal() {
-		let source = &b"e"[..];
-		let index = 0;
-		let byte = 'f' as u8;
-
-		let is_equal = source.is_equal_at(index, byte);
-
-		assert_eq!(is_equal, false);
-	}
-
-	#[test]
-	fn may_not_compare_unreachable() {
-		let source = &b"g"[..];
-		let index = 1;
-		let byte = 'h' as u8;
-
-		let is_equal = source.is_equal_at(index, byte);
-
-		assert_eq!(is_equal, false);
 	}
 }
--- src/abstract_source/str.rs
+++ src/abstract_source/str.rs
@@ -2,21 +2,16 @@ use crate::AbstractSource;
 
 /// This is only available if `str_source` feature has been activated.
 impl<'a> AbstractSource for &'a str {
-	type Slice = &'a str;
-
-	fn forward_slice(&self, start_index: usize) -> Self::Slice {
+	fn forward_slice(self, start_index: usize) -> Self {
 		&self[start_index..]
 	}
 
-	fn is_empty(&self) -> bool {
-		self.len() == 0
+	fn slice(self, start_index: usize, stop_index: usize) -> Self {
+		&self[start_index..stop_index]
 	}
 
-	fn is_equal_at(&self, index: usize, byte: u8) -> bool {
-		self.get(index..index+1)
-			.unwrap_or("")
-			.as_bytes()
-			.get(0) == Some(&byte)
+	fn is_empty_at(&self, index: usize) -> bool {
+		index >= self.len()
 	}
 }
 
@@ -50,38 +45,5 @@ mod t {
 		let is_empty = source.is_empty();
 
 		assert_eq!(is_empty, false);
-	}
-
-	#[test]
-	fn should_be_equal() {
-		let source = "d";
-		let index = 0;
-		let byte = 'd' as u8;
-
-		let is_equal = source.is_equal_at(index, byte);
-
-		assert_eq!(is_equal, true);
-	}
-
-	#[test]
-	fn should_not_be_equal() {
-		let source = "e";
-		let index = 0;
-		let byte = 'f' as u8;
-
-		let is_equal = source.is_equal_at(index, byte);
-
-		assert_eq!(is_equal, false);
-	}
-
-	#[test]
-	fn may_not_compare_unreachable() {
-		let source = "g";
-		let index = 1;
-		let byte = 'h' as u8;
-
-		let is_equal = source.is_equal_at(index, byte);
-
-		assert_eq!(is_equal, false);
 	}
 }
