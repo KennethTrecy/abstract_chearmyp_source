810973b24af7e749513081abfb1e64fcc10606a7
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-18 16:09:15 +0800

    Update the abstract source to use move semantics instead

--- src/abstract_source.rs
+++ src/abstract_source.rs
@@ -1,19 +1,16 @@
 /// An abstraction of source.
 pub trait AbstractSource {
-	/// The type of slice it would return after forward slicing.
-	type Slice;
-
 	/// Creates a forward slice. Equivalent to running `&source[n..]` where `n` is the first argument
 	/// indicating the index where to start slicing.
-	fn forward_slice(&self, _: usize) -> Self::Slice;
+	fn forward_slice(self, _: usize) -> Self;
 
-	/// Check if the source if it still has contents.
-	fn is_empty(&self) -> bool;
+	/// Creates a slice. Equivalent to running `&source[n..m]` where `n` is the first argument
+	/// indicating the index where to start slicing and `m` indicating the index where to stop
+	/// slicing.
+	fn slice(self, _: usize, _: usize) -> Self;
 
-	/// Checks if the has a byte at the targeted index. Equivalent to running `source[n] == m` where
-	/// `n` is the first argument indicating the index to check and `m` is the second argument
-	/// indicating the byte it will be compared to.
-	fn is_equal_at(&self, _: usize, _: u8) -> bool;
+	/// Check if the source if it still has contents at certain offset.
+	fn is_empty_at(&self, _: usize) -> bool;
 }
 
 #[cfg(feature = "str_source")]
